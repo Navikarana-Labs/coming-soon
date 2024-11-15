8c85257258aad6c75527285c14b6afda16ccee4d
Lokesh Kumar <hello@lokeshkr.com>
2024-11-16 03:31:12 +0530

    content updates

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import { FaGithub } from "react-icons/fa";
 import "./app.css";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaAt, FaVoicemail, FaXTwitter } from "react-icons/fa6";
 
 export const App = () => {
   return (
@@ -12,18 +12,36 @@ export const App = () => {
       </div>
       <div className="content">
         <div className="title-holder">
-          <h1>Coming Soon</h1>
-          <p>Website coming soon. Please check back to know more. Shoot us an email if you&apos;re curious.</p>
+          <h1>Website Coming Soon...</h1>
+          <p>
+            Something extraordinary is on the horizon. Join us on this journey.
+            <br />
+            Check back soon for updates, or reach out if you’d like to know
+            more.
+          </p>
         </div>
         <a href="mailto:hello@navikarana.io">
-          <div className="cta">Send us an email</div>
+          <div className="cta">Reach out!</div>
         </a>
         <div className="social">
-          <a href="https://github.com/Navikarana-Labs" title="GitHub" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="icon" />
+          <a href="mailto:hello@navikarana.io" title="Email">
+            <FaAt className="icon" />
           </a>
-          <a href="https://x.com/navikarana_labs" title="Twitter" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://x.com/navikarana_labs"
+            title="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaXTwitter className="icon" />
+          </a>
+          <a
+            href="https://github.com/Navikarana-Labs"
+            title="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub className="icon" />
           </a>
         </div>
       </div>
